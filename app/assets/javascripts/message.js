78dc499726d2b577b1f456120e57b0b2b3faa264
gin10k <gin10k@outlook.jp>
2020-03-23 10:25:15 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,48 +1,48 @@
 $(function(){ 
-      function buildHTML(message){
-      if ( message.image ) {
-        var html =
-          `<div class="main-chat__message__log">
-            <div class="main-chat__message__log__upload">
-              <div class="main-chat__message__log__upload__name">
-                ${message.user_name}
-              </div>
-              <div class="main-chat__message__log__upload__date">
-                ${message.created_at}
-              </div>
+    function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="main-chat__message__log">
+          <div class="main-chat__message__log__upload">
+            <div class="main-chat__message__log__upload__name">
+              ${message.user_name}
             </div>
-            <div class="main-chat__message__log__comment">
-              <p class="main-chat__message__log__comment__content">
-                ${message.content}
-              </p>
+            <div class="main-chat__message__log__upload__date">
+              ${message.created_at}
             </div>
-            <img src=${message.image} >
-          </div>`
-        return html;
-      } else {
-        var html =
-          `<div class="main-chat__message__log">
-            <div class="main-chat__message__log__upload">
-              <div class="main-chat__message__log__upload__name">
-                ${message.user_name}
-              </div>
-              <div class="main-chat__message__log__upload__date">
-                ${message.created_at}
-              </div>
+          </div>
+          <div class="main-chat__message__log__comment">
+            <p class="main-chat__message__log__comment__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="main-chat__message__log">
+          <div class="main-chat__message__log__upload">
+            <div class="main-chat__message__log__upload__name">
+              ${message.user_name}
             </div>
-            <div class="main-chat__message__log__comment">
-              <p class="main-chat__message__log__comment__content">
-                ${message.content}
-              </p>
+            <div class="main-chat__message__log__upload__date">
+              ${message.created_at}
             </div>
-          </div>`
-        return html;
-      };
-      }
-  $("#new_message").on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+          </div>
+          <div class="main-chat__message__log__comment">
+            <p class="main-chat__message__log__comment__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+    }
+    $("#new_message").on('submit', function(e){
+      e.preventDefault()
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -63,5 +63,31 @@ $(function(){
     .always(function(){
       $('.main-chat__form__flame__btn').prop("disabled",false);
     })
-  })
+    })
+    var reloadMessages = function() {
+      var last_message_id = $('.main-chat__message__log:last').data("message-id");
+      console.log(last_message_id);
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main-chat__message').append(insertHTML);
+      $('.main-chat__message').animate({ scrollTop: $('.main-chat__message')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+    if (document.location.href.match(/\/groups\/messages/)) {
+      setInterval(reloadMessages, 7000);
+    }
 });
