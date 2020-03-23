1635c99a449db2c38c349708decb95fe9a6f0103
gin10k <gin10k@outlook.jp>
2020-03-23 10:37:03 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -66,7 +66,6 @@ $(function(){
     })
     var reloadMessages = function() {
       var last_message_id = $('.main-chat__message__log:last').data("message-id");
-      console.log(last_message_id);
       $.ajax({
         url: "api/messages",
         type: 'get',
